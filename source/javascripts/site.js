f0c9ed8ac19b89a73f2698635e3672cbc30022b4
Christopher David Yudichak <TheOddLinguist@gmail.com>
2019-01-08 21:08:13 -0500

    add captcha

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -12,33 +12,43 @@ $(window).scroll(function() {
 document.addEventListener("DOMContentLoaded", function(event) {
   document.querySelectorAll("[data-behavior='contact-form']").forEach(function(form) {
     form.addEventListener("submit", function(event) {
+      const contactForm = this
       event.preventDefault();
-      let formData = new FormData(this);
-      let parsedData = {
-        ticket: {
-          name:        formData.get("ticket[name]"),
-          email:       formData.get("ticket[email]"),
-          inquiryType: formData.get("ticket[inquiry_type]"),
-          app:         formData.get("ticket[app]"),
-          source:      formData.get("ticket[source]"),
-          subject:     formData.get("ticket[subject]"),
-          message:     formData.get("ticket[message]"),
-        }
-      }
+      grecaptcha.ready(function() {
+        grecaptcha
+          .execute('6Lf-EIgUAAAAACtqE96q1QSsHT0B72S4NLAQi7_g', {action: 'contactForm'})
+          .then(function(token, i) {
+            let formData = new FormData(contactForm);
+            let parsedData = {
+              captcha: {
+                response: token,
+              },
+              ticket: {
+                name:        formData.get("ticket[name]"),
+                email:       formData.get("ticket[email]"),
+                inquiryType: formData.get("ticket[inquiry_type]"),
+                app:         formData.get("ticket[app]"),
+                source:      formData.get("ticket[source]"),
+                subject:     formData.get("ticket[subject]"),
+                message:     formData.get("ticket[message]"),
+              }
+            }
 
-      let options = {
-        body: JSON.stringify(parsedData),
-        method: 'post',
-        mode: 'cors',
-        headers:{
-          'Content-Type': 'application/json',
-        },
-      };
+            let options = {
+              body: JSON.stringify(parsedData),
+              method: 'post',
+              mode: 'cors',
+              headers:{
+                'Content-Type': 'application/json',
+              },
+            };
 
-      fetch("https://hello.howseven.com", options).then(r => r.json()).then(data => {
-        this.reset()
-        this.style.display = "none"
-        document.getElementsByClassName("form-success")[0].style.display = "block"
+            fetch("https://hello.howseven.com", options).then(r => r.json()).then(data => {
+              contactForm.reset()
+              contactForm.style.display = "none"
+              document.getElementsByClassName("form-success")[0].style.display = "block"
+            });
+          });
       });
     })
   })
